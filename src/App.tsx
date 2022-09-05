b869b27213a83eaa8e1ed3fed24c18c1777f4963
Gabriel Godoy <gabrielgodoyon@gmail.com>
2022-09-05 12:25:03 -0300

    feat: add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Gabriel Godoy</p>
+        <h2>Gabriel Godoy</h2>
+        <p>Front-end developer</p>
+        <div className="icons">
+          <a
+            href="https://www.linkedin.com/in/gabrielgodoy1/"
+            target="_blank"
+            className="icon"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/gabrielgodoy"
+            target="_blank"
+            className="icon"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="mailto: gabrielgodoyon@gmail.com"
+            target="_blank"
+            className="icon"
+            rel="noreferrer"
+          >
+            <FaEnvelope />
+          </a>
+        </div>
       </header>
     </div>
   )
